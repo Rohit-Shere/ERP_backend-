aa394cc3c2239c624766ac6d17c92333be25b200
Rohit <rhtshere@gmail.com>
2026-09-12 14:07:23 +0530

    added origins

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://leaveapplication-seven.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],   # GET, POST, PUT, DELETE, OPTIONS
